8efa3b71f730494cc8a9695488646a036eba07b6
MisterPeddler <mr.padli@gmail.com>
2017-02-21 00:40:05 -0800

    added static html with some css for latest adventures section

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -18,8 +18,41 @@ get_header(); ?>
 
   </section>
 
-<!-- more stuff goes here -->
+<!-- more stuff goes here -> static content-->
+<!-- <div class="container"> -->
 
+<section class="latest-adventures">
+
+<h1>LATEST ADVENTURES</h1>
+
+<div class="adventure-container">
+<div class="adventure-left">
+  <h2>Getting Back to Nature in a Canoe</h2>
+  <a href="#" class="read-more">READ MORE</a>
+</div>
+
+<div class="adventure-right-container">
+  <div class="adventure-top-right">
+    <h2>A Night with Friends at the Beach</h2>
+    <a href="#" class="read-more">READ MORE</a>
+  </div>
+
+  <div class="adventure-bottom-left">
+    <h2>Taking in the View at Big Mountain</h2>
+    <a href="#" class="read-more">READ MORE</a>
+  </div>
+
+  <div class="adventure-bottom-right">
+    <h2>Star-Gazing at the Night Sky</h2>
+    <a href="#" class="read-more">READ MORE</a>
+  </div>
+</div>
+</div>
+
+</section>
+<!-- </div> -->
+
+<!-- end of static content -->
 </section>
 
 <?php get_footer(); ?>
