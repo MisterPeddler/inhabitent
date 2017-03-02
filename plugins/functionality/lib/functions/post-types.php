373e3aa9dc4b767f487d37747753c9bbd11f5756
MisterPeddler <mr.padli@gmail.com>
2017-03-02 12:00:04 -0800

    edited git ignore to add functionality plugin

---
+++ plugins/functionality/lib/functions/post-types.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * POST TYPES
+ *
+ * @link  http://codex.wordpress.org/Function_Reference/register_post_type
+ */
+
+// Add your custom post types here...
+
+// Register Product Post Type
+function inhabitent_cpt_products() {
+
+	$labels = array(
+		'name'                  => 'Products',
+		'singular_name'         => 'Products',
+		'menu_name'             => 'Products',
+		'name_admin_bar'        => 'Products',
+		'archives'              => 'Product Archives',
+		'attributes'            => 'Product Attrbutes',
+		'parent_item_colon'     => 'Parent Product:',
+		'all_items'             => 'All Products',
+		'add_new_item'          => 'Add New Product',
+		'add_new'               => 'Add New Product',
+		'new_item'              => 'New Product',
+		'edit_item'             => 'Edit Product',
+		'update_item'           => 'Update Product',
+		'view_item'             => 'View Product',
+		'view_items'            => 'View Products',
+		'search_items'          => 'Search Product',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into product',
+		'uploaded_to_this_item' => 'Uploaded to this item',
+		'items_list'            => 'Products list',
+		'items_list_navigation' => 'Products list navigation',
+		'filter_items_list'     => 'Filter products list',
+	);
+	$args = array(
+		'label'                 => 'Products',
+		'description'           => 'Listing of items to sell',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-store',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'products', $args );
+
+}
+add_action( 'init', 'inhabitent_cpt_products', 0 );
+
+
+// Register Custom Post Type
+function adventures() {
+
+	$labels = array(
+		'name'                  => 'Adventures',
+		'singular_name'         => 'Adventure',
+		'menu_name'             => 'Adventures',
+		'name_admin_bar'        => 'Adventure',
+		'archives'              => 'Adventure Archives',
+		'attributes'            => 'Adventure Attributes',
+		'parent_item_colon'     => 'Parent Item:',
+		'all_items'             => 'All Adventures',
+		'add_new_item'          => 'Add New Adventure',
+		'add_new'               => 'Add New Adventure',
+		'new_item'              => 'New Adventure',
+		'edit_item'             => 'Edit Adventure',
+		'update_item'           => 'Update Adventure',
+		'view_item'             => 'View Adventure',
+		'view_items'            => 'View Adventures',
+		'search_items'          => 'Search Adventures',
+		'not_found'             => 'Adventure Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into Adventure',
+		'uploaded_to_this_item' => 'Uploaded to this Adventure',
+		'items_list'            => 'Adventures list',
+		'items_list_navigation' => 'Adventures list navigation',
+		'filter_items_list'     => 'Filter Adventures list',
+	);
+	$args = array(
+		'label'                 => 'Adventure',
+		'description'           => 'An Amazing Adventure',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'revisions', ),
+		'taxonomies'            => array(),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-thumbs-up',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'adventure', $args );
+
+}
+add_action( 'init', 'adventures', 0 );
